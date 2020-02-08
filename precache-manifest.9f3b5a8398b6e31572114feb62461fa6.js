bcf9b24c80e896ec95cac80cc936a8e96a9521e7
joel sithivong <joelsithivong@MacBook-Pro-de-joel.local>
2020-02-08 10:52:33 +0100

    Updates

---
+++ precache-manifest.9f3b5a8398b6e31572114feb62461fa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981348b71be34de671bc495ef8e2e4df",
+    "url": "/appcontacts/index.html"
+  },
+  {
+    "revision": "194f0e463d41063d875c",
+    "url": "/appcontacts/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "ed008bf3e67b1fab89ec",
+    "url": "/appcontacts/static/css/main.707b2027.chunk.css"
+  },
+  {
+    "revision": "194f0e463d41063d875c",
+    "url": "/appcontacts/static/js/2.0b3c013f.chunk.js"
+  },
+  {
+    "revision": "ed008bf3e67b1fab89ec",
+    "url": "/appcontacts/static/js/main.ca3637f1.chunk.js"
+  },
+  {
+    "revision": "8b5858ddd3b62b9be9a4",
+    "url": "/appcontacts/static/js/runtime-main.ae722a46.js"
+  }
+]);
